8d3f5a5128a393d08e302b6be2c7fddf3b04dd22
mrrezaahmadi <mrrezaahmadi>
2020-06-10 15:33:59 +0430

    add render function to render notes on page

---
+++ script.js
@@ -0,0 +1,33 @@
+const notes = [{
+    title: 'Got a new email',
+    body: 'I should got a new email by the noon.'
+}, {
+    title: 'Food!!!',
+    body: 'gotta eat something for the dinner'
+}, {
+    title: 'My university fees',
+    body: 'university fees has to paid off before next semester'
+}, {
+    title: 'Watering the garden',
+    body: 'the garden seems so dry, need some water!'
+}];
+
+const filters = {
+    searchText: ''
+};
+
+const render = (notes, filters) => {
+    const filteredNotes = notes.filter(note => {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ``
+
+    filteredNotes.forEach(note => {
+        const p = document.createElement('p')
+        p.textContent = note.title;
+        document.querySelector('#notes').appendChild(p)
+    });
+}
+
+render(notes, filters)
